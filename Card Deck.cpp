987a9cf9be45653e31e425d27aecb3db653e43fb
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-03-05 13:36:40 +0530

    Create Card Deck.cpp

---
+++ Card Deck.cpp
@@ -0,0 +1,25 @@
+#import<bits/stdc++.h>
+using namespace std;
+int m,n,t,a[100005],p[100005];
+main()
+{
+	for(cin>>t;t--;)
+	{
+		cin>>n;
+		for(int i=1;i<=n;i++)
+		{
+			cin>>a[i];
+			p[a[i]]=i;
+		}
+		m=n+1;
+		for(int i=n;i;i--)
+		{
+			if(p[i]<m)
+			{
+				for(int j=p[i];j<m;j++)cout<<a[j]<<' ';
+				m=p[i];
+			}
+		}
+		cout<<endl;
+	}
+}
