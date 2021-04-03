9ac9e36765f615ebcff78f0abaab263886be3646
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-04-03 16:19:02 +0530

    Create Valid Pair.cpp

---
+++ April-Challenge/Valid Pair.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define REP(i,n) for (int i = 1; i <= n; i++)
+#define mod 1000000007
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<int,int>
+#define vii vector<ii>
+#define lli long long int
+#define ll long 
+#define INF 1000000000;
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define endl '\n'
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b || b==c || a==c)
+    {
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    
+}
