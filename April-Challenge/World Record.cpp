98723e1e1fcee96cb7233134a91fc53d16c6cb3e
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-04-13 11:15:49 +0530

    Create World Record.cpp

---
+++ April-Challenge/World Record.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define REP(i,n) for (int i = 1; i <= n; i++)
+#define mod 1000000007
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<int,int>
+#define vii vector<ii>
+#define lli long long int
+#define ll long 
+#define INF 1000000000;
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define endl '\n'
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        float v1,v2,v3,s;
+        cin>>v1>>v2>>v3>>s;
+        // float res = v1*v2*v3*s;
+        float ans = 100/(v1*v2*v3*s);
+        float check = 9.575;
+        if(ans<check)
+        {
+            cout<<"YES";
+        }
+        else
+        {
+            cout<<"NO";
+        }
+        cout<<endl;
+        
+    }
+    
+}
