7545f0b2e3ddc82c99325c190089ca02ae2962de
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-04-16 22:47:14 +0530

    Create A - Average Height.cpp

---
+++ Codeforces Round 715 (Div. 2)/A - Average Height.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	
+	
+	int t;
+       cin>>t;
+       while(t--)
+       {
+           int n;
+		   cin>>n;
+		//    int arr[n];
+		   vector<int>o;
+		   vector<int>e;
+		   for(int i=0;i<n;i++)
+		   {
+			   int it;
+			   cin>>it;
+			   if(it%2==0)
+			   {
+				   e.push_back(it);
+			   }
+			   else{
+				   o.push_back(it);
+			   }
+		   }
+		//    int j=0;
+		   for(int i=0;i<o.size();i++)
+		   {
+			   cout<<o[i]<<" ";
+		   }
+		   for(int i=0;i<e.size();i++)
+		   {
+			   cout<<e[i]<<" ";
+			   
+		   }
+		   cout<<endl;
+		   
+ 
+ 
+ 
+ 
+ 
+       }
+}
+
+
+    
