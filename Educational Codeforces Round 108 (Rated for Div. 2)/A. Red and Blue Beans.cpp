df84d3c654562afe34d6652663fdfc43aab1631b
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-04-29 22:46:00 +0530

    Create A. Red and Blue Beans.cpp

---
+++ Educational Codeforces Round 108 (Rated for Div. 2)/A. Red and Blue Beans.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+using namespace std;
+
+void solve()
+{
+	
+	// 0 0 0 0 0
+	// 0 0 0 0 0
+	// 0 0 1 0 0
+	// 0 0 0 0 0
+	// 0 0 0 0 0
+ll a,b,c;
+cin>>a>>b>>c;
+ll m1=min(a,b);
+// ll m=max(a,b);
+long long res=(c+1)*m1;
+if(res>=max(a,b))
+{
+    cout<<"YES";
+}
+else{
+    cout<<"NO";
+}
+
+
+	
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	
+
+	int t;
+	cin>>t;
+	//  int t = 1;
+	while(t--)
+	{
+		solve();
+		cout<<el;
+	}
+
+
+}
+
+
+    
