a0cf059d272689e072a8290e23ce104828b4622f
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-05-21 00:32:06 +0530

    Create And Then There Were K.cpp

---
+++ Codeforces Round #721 (Div. 2)/And Then There Were K.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+using namespace std;
+void solve()
+{
+	
+	// 0 0 0 0 0
+	// 0 0 0 0 0
+	// 0 0 1 0 0
+	// 0 0 0 0 0
+	// 0 0 0 0 0
+	int n;
+	cin>>n;
+	if(n==1)
+	{
+		cout<<0;
+	}
+	else{
+	int ans = 1;
+	while(ans*2<=n)
+	{
+		ans=ans*2;
+	}
+	cout<<ans-1;
+	
+	}
+	
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	
+
+	int t;
+	cin>>t;
+	// t = 1;
+	while(t--)
+	{
+		solve();
+		cout<<el;
+	}
+
+
+}
+
+
+    
