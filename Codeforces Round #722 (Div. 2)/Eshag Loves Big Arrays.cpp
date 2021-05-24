b029e74bfe23c271be11e89c57d7ae7ef20ad789
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-05-25 00:04:25 +0530

    Create Eshag Loves Big Arrays.cpp

---
+++ Codeforces Round #722 (Div. 2)/Eshag Loves Big Arrays.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+using namespace std;
+// int vis[100001];
+void solve()
+{
+	
+	// 0 0 0 0 0
+	// 0 0 0 0 0
+	// 0 0 1 0 0
+	// 0 0 0 0 0
+	// 0 0 0 0 0
+	/*
+	istringstream ss(str);
+    string word; 
+	vector<string>ans;
+    while(ss >> word) 
+        ans.push_back(word);
+    }
+
+	*/
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	sort(arr,arr+n);
+	int c= 0;
+	int sum = 0;
+	for(int i=0;i<n;i++)
+	{
+		sum+=arr[i];
+	}
+	int i = n-1;
+	int x = n;
+	while(i>=0)
+	{
+		int temp = sum/x;
+		if(temp>=arr[i])
+		{
+			break;
+		}
+		else{
+			c++;
+			sum = sum-arr[i];
+			i--;
+			x--;
+			
+		}
+	}
+	cout<<c;
+	
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	
+
+	int t;
+	cin>>t;
+	//  t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		solve();
+		cout<<el;
+	}
+
+
+}
+
+
+    
