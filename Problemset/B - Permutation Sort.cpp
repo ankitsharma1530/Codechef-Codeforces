fc6e07cd1f0d7c2ba699e4510fae940eed2baa48
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-05-26 12:24:20 +0530

    Create B - Permutation Sort.cpp

---
+++ Problemset/B - Permutation Sort.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+using namespace std;
+// int vis[100001];
+void solve()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	bool flag = false;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+		if(arr[i]!=i+1)
+		{
+			flag=true;
+		}
+	}
+	if(!flag){
+		cout<<0;
+	}
+	else{
+		if(arr[0]!=1 and arr[n-1]!=n){
+			if(arr[0]==n and arr[n-1]==1){
+				cout<<3;
+			}
+			else{
+			cout<<2;
+			}
+		}
+		else{
+			cout<<1;
+		}
+	}
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	
+
+	int t;
+	cin>>t;
+	//  t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		solve();
+		cout<<el;
+	}
+
+
+}
+
+
+    
