52f669b93b0eeff37c552f17c15b4f556a74ce27
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-06-21 20:38:46 +0530

    Create 1263A - Sweet Problem.cpp

---
+++ Problemset/1263A - Sweet Problem.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+using namespace std;
+// int vis[100001];
+
+void solve()
+{
+	// O(n^2)
+	priority_queue<int>pq;
+	for(int i=0;i<3;i++){
+		int it;
+		cin>>it;
+		pq.push(it);
+	}
+	int res = 0;
+	int m1 = pq.top();
+	pq.pop();
+	int m2 = pq.top();
+	pq.pop();
+	int m3 = pq.top();
+	pq.pop();
+
+	if(m1>=m2+m3){
+		cout<<m3+m2;
+		return;
+	}
+	else{
+		cout<<m1+((m2+m3-m1)/2);
+	}
+
+
+	// cout<<res;
+
+
+	// YES
+	// RSRRSRSSSR
+
+
+
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	
+
+	int t;
+	cin>>t;
+	//  t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		solve();
+		cout<<el;
+	}
+
+
+}
+
+
+    
