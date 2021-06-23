240bee7bc175f6fc435a8f6727c725f2f776a4c8
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-06-23 13:17:05 +0530

    Create 1200B - Block Adventure.cpp

---
+++ Problemset/1200B - Block Adventure.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+using namespace std;
+// int vis[100001];
+
+void solve()
+{
+	int n,m,k;
+	cin>>n>>m>>k;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	// |h(i)−h(i+1)|≤k
+	for(int i=0;i<n-1;i++){
+		
+		int d = arr[i+1]-k;
+		int q = 0;
+		if(d>0){
+			q=d;
+		}
+		m+=arr[i]-q;
+		if(m<0){
+			cout<<"NO"<<endl;
+			return;
+		}
+	}
+	cout<<"YES"<<endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	
+
+	int t;
+	cin>>t;
+	//  t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		solve();
+		// cout<<el;
+	}
+
+
+}
+
+
+    
