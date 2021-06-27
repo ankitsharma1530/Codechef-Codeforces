8992cbb98214aaa2ce87f0a6dd7c808248e69a5a
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-06-27 23:31:11 +0530

    Create 1006C - Three Parts of the Array.cpp

---
+++ 1006C - Three Parts of the Array.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+/*
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+	cin>>arr[i];
+}
+*/
+using namespace std;
+void solve()
+{
+	int n;
+	cin>>n;
+	int a[n+1];
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+	}
+	int i = 0, j = n+1;
+	ll zi = 0, zj = 0, solidx = 0;
+	while (i < j) {
+		if (zi < zj)
+			zi += a[++i];
+		else if (zi > zj)
+			zj += a[--j];
+		else {
+			solidx = i;
+			zi += a[++i];
+			zj += a[--j];
+		}
+	}
+	ll ans = 0;
+	for(int  i=1;i<=solidx;i++){
+		ans+=a[i];
+	}
+	cout<<ans;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+
+	int t;
+	// cin>>t;
+	 t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		solve();
+		// cout<<el;
+	}
+
+
+}
+
+
+    
