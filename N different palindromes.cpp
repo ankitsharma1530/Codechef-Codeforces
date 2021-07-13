b16c085074f827efd4b9be28ac404e304e944114
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-07-13 20:00:08 +0530

    Create N different palindromes.cpp

---
+++ N different palindromes.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+/*
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+	cin>>arr[i];
+}
+*/
+using namespace std;
+void solve()
+{
+	int n;
+	cin>>n;
+	string res = "";
+	int i=0;
+	for(int j=0;j<n;j++){
+		if(i==0){
+			res+='a';
+			i++;
+		}
+		else if(i==1){
+			res+='b';
+			i++;
+		}
+		else{
+			res+='c';
+			i=0;
+		}
+	}
+	cout<<res<<endl;
+	
+	
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+
+	int t;
+	cin>>t;
+	//  t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		solve();
+		// cout<<el;
+	}
+
+
+}
+
+
+    
