c67aba9b9479c8b30f119b48f6d869946619c215
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-07-20 16:54:46 +0530

    Create C. Canine poetry.cpp

---
+++ Problemset/C. Canine poetry.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+/*
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+	cin>>arr[i];
+}
+*/
+using namespace std;
+
+void solve()
+{
+	string s;
+	cin>>s;
+	// s.push_back('*');
+	int c = 0;
+	int n = s.length();
+	bool used[n];
+	memset(used,false,sizeof(used));
+	for(int i=1;i<n;i++){
+		bool is = false;
+		if(s[i]==s[i-1] and !used[i-1]){
+			is = true;
+		}
+		if(i>1 and s[i]==s[i-2] and !used[i-2]){
+			is = true;
+		}
+		used[i] = is;
+		c+=used[i];
+	}
+	cout<<c<<endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	
+	int t;
+	cin>>t;
+	//  t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		solve();
+		// cout<<el;
+	}
+
+
+}
+
+
+    
