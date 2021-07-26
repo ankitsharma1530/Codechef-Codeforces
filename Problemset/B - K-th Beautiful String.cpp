22b2548b773223490bfcac1d437b5562d44e813b
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-07-26 23:24:22 +0530

    Create 	B - K-th Beautiful String.cpp

---
+++ Problemset/B - K-th Beautiful String.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+/*
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+	cin>>arr[i];
+}
+*/
+using namespace std;
+void solve()
+{
+    ll n, k;
+        cin >> n >> k;
+        string s(n, 'a');
+        for (int i = n - 2; i >= 0; i--) {
+            if (k <= (n - i - 1)) {
+                s[i] = 'b';
+                s[n - k] = 'b';
+                cout << s << "\n";
+                break;
+            }
+            k -= (n - i - 1);
+        }
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	
+	int t;
+	cin>>t;
+	//  t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		solve();
+		// cout<<el;
+	}
+
+
+}
+
+
+    
