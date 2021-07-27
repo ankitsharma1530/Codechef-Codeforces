a5981beb3f64420087c27c1793a3eae5b88c5320
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-07-27 12:59:15 +0530

    Create 1335D - Anti-Sudoku.cpp

---
+++ Problemset/1335D - Anti-Sudoku.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+#define INF 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+/*
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+	cin>>arr[i];
+}
+*/
+using namespace std;
+void solve()
+{
+    // int n;
+    // cin>>n;
+    char arr[9][9];
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cin>>arr[i][j];
+            if(arr[i][j]=='1'){
+                arr[i][j] = '2';
+            }
+        }
+    }
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	
+	int t;
+	cin>>t;
+	//  t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		solve();
+		// cout<<el;
+	}
+
+
+}
+
+
+    
