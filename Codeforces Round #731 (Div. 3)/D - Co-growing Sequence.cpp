f96c510b2241ec00a8874f1a0a9e5e146eda76e2
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-08-05 15:49:39 +0530

    Create D - Co-growing Sequence.cpp

---
+++ Codeforces Round #731 (Div. 3)/D - Co-growing Sequence.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define lli long long int
+#define ll long long
+#define pll pair<long long,long long>
+const int INF = 1000000000;
+#define el '\n' 
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// for loops shortcut
+#define garr(i, n)       for(ll i = 0; i < (n); i++)
+#define parr(i, n, arr)       for(ll i = 0; i < (n); i++) cout<<arr[i]<<" ";
+/*
+vector<ll>arr;
+for(int i=0;i<n;i++)
+{
+	ll it;
+	cin>>it;
+	arr.push_back(it);
+}
+*/
+/*
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+	cin>>arr[i];
+}
+*/
+using namespace std;
+int arr[26];
+void solve()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	int res[n];
+	res[0] = 0;
+	for(int i=1;i<n;i++){
+		int p = res[i-1]^arr[i-1];
+		int num = 0;
+		int curr = arr[i];
+		for(int j=0;j<30;j++){
+			if((1<<j)&p){
+			if(curr&(1<<j)){
+				// if it is set bit
+				// continue;
+			}
+			else{
+				// make the bit set
+				num = num|(1<<j);
+			}
+			}
+		}
+		res[i] = num;
+	}
+	for(int i=0;i<n;i++){
+		cout<<res[i]<<" "; 
+	}
+	cout<<endl;
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	
+	int t;
+	cin>>t;
+	//  t = 1;
+	while(t--)
+	{
+		// memset(vis,0,sizeof(vis));
+		// memset(dp,-1,sizeof(dp));
+		solve();
+		// cout<<el;
+	}
+
+
+}
+
+
+    
